fa0b8717b2f3a024155ed8692e4208157d546362
shierro <theodoro.pelingan@gmail.com>
2017-09-11 01:11:12 +0800

    added vuetify to project. also, added a drawer and toolbar

---
+++ src/components/Main.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app id="example-1" toolbar footer>
+    <v-navigation-drawer
+      persistent
+      v-model="drawer"
+      light
+      enable-resize-watcher
+      overflow
+      :mini-variant.sync="mini"
+    >
+      <v-toolbar flat class="transparent">
+        <v-list class="pa-0">
+          <v-list-tile avatar tag="div">
+            <v-list-tile-avatar>
+              <img src="../assets/picture.jpg" />
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>Theo</v-list-tile-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-btn icon @click.native.stop="toggleDrawer()">
+                <v-icon>chevron_left</v-icon>
+              </v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+    </v-navigation-drawer>
+    <v-toolbar class="indigo" dark>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>{{ toolbarTitle }}</v-toolbar-title>
+    </v-toolbar>
+    <main>
+      <v-container fluid>
+        <!--v-router-->
+      </v-container>
+    </main>
+    <v-footer class="indigo">
+      <span class="white--text">© 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      drawer: true,
+      mini: false,
+      toolbarTitle: 'Professional Resume'
+    }
+  },
+  methods: {
+    toggleDrawer () {
+      this.drawer = !this.drawer
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'vuetify/dist/vuetify.min.css'
+import './assets/material-design-icons-2.2.0/iconfont/material-icons.css'
+
 import Vue from 'vue'
+import Vuetify from 'vuetify'
+
 import App from './App'
 import router from './router'
 
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
