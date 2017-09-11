f87f5fb07426cbbf2acd1946bb0f820815f937c5
shierro <theodoro.pelingan@gmail.com>
2017-09-11 11:26:21 +0800

    added list of navigation added section for picture, name and job title made drawer permanent

---
+++ src/components/Drawer.vue
@@ -0,0 +1,135 @@
+<template>
+  <v-navigation-drawer
+    persistent
+    light
+    enable-resize-watcher
+    overflow
+    permanent
+    v-scroll="vScroll"
+  >
+    <v-card elevation-3 class="drawer__card">
+      <v-avatar class="drawer__card--avatar">
+        <img src="../assets/picture.jpg" />
+      </v-avatar>
+      <span class="drawer__card--name">
+        {{ fullName }}
+      </span>
+      <span class="drawer__card--title">
+        {{ jobTitle }}
+      </span>
+    </v-card>
+    <v-list two-line class="drawer__nav-list">
+      <template v-for="(nav, index) in navList">
+        <v-list-tile avatar ripple v-bind:key="index">
+          <v-list-tile-content>
+            <v-list-tile-title class="drawer__nav-list--title">
+              <i :class="nav.class">{{nav.icon }}</i>
+              <span>{{ nav.title }}</span>
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-divider v-if="index + 1 < navList.length" v-bind:key="index"></v-divider>
+      </template>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    name: {
+      first: 'Theodoro',
+      middleInitial: 'S.',
+      last: 'Pelingan',
+      suffix: 'Jr.'
+    },
+    jobTitle: 'Fullstack Web Developer',
+    navList: [
+      {
+        title: 'Employment',
+        icon: 'assignment',
+        class: 'material-icons icon green--text text--darken-2 icon--large'
+      },
+      {
+        title: 'Education',
+        icon: 'school',
+        class: 'material-icons icon blue--text text--darken-2 icon--large'
+      },
+      {
+        title: 'Technical Expertise',
+        icon: 'business',
+        class: 'material-icons icon yellow--text text--darken-2 icon--large'
+      },
+      {
+        title: 'Language and Technologies',
+        icon: 'code',
+        class: 'material-icons icon red--text text--darken-2 icon--large'
+      }
+    ],
+    vScroll: {
+      callback: function () {
+        console.log('test')
+      },
+      target: 'div#app'
+    }
+  }),
+  computed: {
+    fullName: function () {
+      return `${this.name.first} ${this.name.middleInitial} ${this.name.last} ${this.name.suffix}`
+    }
+  },
+  methods: {
+    onScroll (e) {
+      console.log(e)
+    },
+    highlightNavItem () {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+ .drawer {
+   &__card {
+    display: block;
+    align-items: center;
+    justify-content: center;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    text-align: center;
+
+    &--avatar{
+      height: 100px !important;
+      width: auto !important;
+      display: block;
+      
+      img {
+        border-style: groove;
+      }
+    }
+
+    &--name {
+      font-weight: 600;
+      font-size: 1.2em;
+      display: block;
+    }
+   }
+
+   &__nav-list {
+    padding: 0px;
+
+    &--title {
+      height: 30px;
+      line-height: 30px;
+      cursor: pointer;
+
+      i {
+        margin-bottom: 5px;
+      }
+    }
+   }
+ }
+</style>
+
+
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,60 +1,34 @@
 <template>
   <v-app id="example-1" toolbar footer>
-    <v-navigation-drawer
-      persistent
-      v-model="drawer"
-      light
-      enable-resize-watcher
-      overflow
-      :mini-variant.sync="mini"
-    >
-      <v-toolbar flat class="transparent">
-        <v-list class="pa-0">
-          <v-list-tile avatar tag="div">
-            <v-list-tile-avatar>
-              <img src="../assets/picture.jpg" />
-            </v-list-tile-avatar>
-            <v-list-tile-content>
-              <v-list-tile-title>Theo</v-list-tile-title>
-            </v-list-tile-content>
-            <v-list-tile-action>
-              <v-btn icon @click.native.stop="toggleDrawer()">
-                <v-icon>chevron_left</v-icon>
-              </v-btn>
-            </v-list-tile-action>
-          </v-list-tile>
-        </v-list>
-      </v-toolbar>
-    </v-navigation-drawer>
+    <drawer></drawer>
     <v-toolbar class="indigo" dark>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title>{{ toolbarTitle }}</v-toolbar-title>
     </v-toolbar>
     <main>
       <v-container fluid>
-        <!--v-router-->
       </v-container>
     </main>
     <v-footer class="indigo">
-      <span class="white--text">© 2017</span>
+      <span class="white--text">{{ credits }}</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
+import Drawer from './Drawer'
 
 export default {
   data () {
     return {
-      drawer: true,
-      mini: false,
-      toolbarTitle: 'Professional Resume'
+      toolbarTitle: 'Professional Resume',
+      credits: '© 2017'
     }
   },
-  methods: {
-    toggleDrawer () {
-      this.drawer = !this.drawer
-    }
+  components: {
+    Drawer
   }
 }
 </script>
+<style lang="scss" scoped>
+</style>
+
